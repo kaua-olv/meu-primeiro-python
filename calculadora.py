09db5cdf3446b0651185b019a9c6bc028d93afe5
Kauã Oliveira <kauabarros0816@gmail.com>
2026-02-26 23:43:52 -0300

    minha primeira calculadora de soma.

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+print('CALCULADORA')
+
+print('seja bem-vindo a calculadora')
+num1= int(input('Digite um numero: '))
+num2= int(input('Digite outro numero: '))
+resultado = num1 + num2
+print ('resultado: ',resultado)
